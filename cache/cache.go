956beea7e397b28d83c9665afb04e8f7b31badc4
wenpp <wen_pp@163.com>
2020-03-17 17:32:13 +0800

    Complete Cache Module

---
+++ cache/cache.go
@@ -0,0 +1,73 @@
+package cache
+
+import (
+	"container/list"
+)
+
+type Cache struct {
+	//字典表，用于映射key和链表的element
+	dict map[string]*list.Element
+	//双向链表,链表头部是最长时间没有使用的entry,尾部是最近使用的
+	dll *list.List
+	//缓存的最大容量
+	maxSize int64
+	//缓存的当前容量
+	currSize int64
+}
+
+type Value interface {
+	Len() int
+}
+
+type Entry struct {
+	key   string
+	value Value
+}
+
+func (cache *Cache) Len() int {
+	return cache.dll.Len()
+}
+
+func Init(maxSize int64) *Cache {
+	return &Cache{
+		maxSize: maxSize,
+		dll:     list.New(),
+		dict:    make(map[string]*list.Element),
+	}
+}
+
+func (cache *Cache) Put(key string, value Value) {
+	if element, ok := cache.dict[key]; ok {
+		cache.dll.MoveToFront(element)
+		entry := element.Value.(*Entry)
+		entry.value = value
+	} else {
+		element := cache.dll.PushFront(&Entry{key, value})
+		cache.dict[key] = element
+		cache.currSize += int64(len(key)) + int64(value.Len())
+	}
+
+	//初始化的时候如果为0则不限制大小，永不删除最久没有使用的entry
+	if cache.maxSize != 0 && cache.maxSize < cache.currSize {
+		cache.DeleteOldest()
+	}
+}
+
+func (cache *Cache) Get(key string) (value Value, ok bool) {
+	if element, ok := cache.dict[key]; ok {
+		cache.dll.MoveToBack(element)
+		entry := element.Value.(*Entry)
+		return entry.value, true
+	}
+	return
+}
+
+func (cache *Cache) DeleteOldest() {
+	element := cache.dll.Back()
+	if element != nil {
+		cache.dll.Remove(element)
+		entry := element.Value.(*Entry)
+		delete(cache.dict, entry.key)
+		cache.currSize -= int64(len(entry.key)) + int64(entry.value.Len())
+	}
+}
---
+++ test/cache_test.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"Cache/cache"
+	"testing"
+)
+
+type String string
+
+func (s String) Len() int {
+	return len(s)
+}
+
+func TestPut(t *testing.T) {
+	cache := cache.Init(int64(0))
+	cache.Put("key", String("Hello"))
+
+	expect := 1
+
+	if expect != cache.Len() {
+		t.Fatal("expected 1 but got", cache.Len())
+	}
+}
+
+func TestGet(t *testing.T) {
+	cache := cache.Init(int64(0))
+	cache.Put("key1", String("Hello"))
+	cache.Put("key2", String("morning"))
+
+	expect1 := String("Hello")
+	expect2 := String("morning")
+
+	if entry, _ := cache.Get("key1"); entry != expect1 {
+		t.Fatal("expected Hello but got", entry)
+	}
+
+	if entry, _ := cache.Get("key2"); entry != expect2 {
+		t.Fatal("expected morning but got", entry)
+	}
+}
+
+func TestDeleteOldest(t *testing.T) {
+	key1 := "key1"
+	key2 := "key2"
+	key3 := "key3"
+
+	value1 := String("value1")
+	value2 := String("value2")
+	value3 := String("value=3")
+
+	maxSizeCorrect := int64(len(key1) + len(key2) + value1.Len() + value2.Len())
+	cache := cache.Init(maxSizeCorrect)
+
+	cache.Put(key1, value1)
+	cache.Put(key2, value2)
+	cache.Put(key3, value3)
+
+	if _, ok := cache.Get(key1); ok {
+		t.Fatal("Key1 still existed")
+	}
+
+	expectEntryLen := 2
+
+	if cache.Len() != expectEntryLen {
+		t.Fatal("expect cache size is 2 but got", cache.Len())
+	}
+
+}
